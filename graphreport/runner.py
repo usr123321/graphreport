6fe36ee4b6f1f160dd55785ce6b6bbf8fe2a0e36
shambkum <shambkum@cisco.com>
2020-03-10 13:43:27 +0530

    new file:   graphreport/graphreport.py 	new file:   graphreport/keyword_stats.py 	new file:   graphreport/runner.py 	new file:   graphreport/test_results.py 	new file:   graphreport/test_stats.py 	new file:   graphreport/version.py 	deleted:    graph-report/graphreport.py 	deleted:    graph-report/keyword_stats.py 	deleted:    graph-report/runner.py 	deleted:    graph-report/test_results.py 	deleted:    graph-report/test_stats.py 	deleted:    graph-report/version.py 	modified:   setup.py

---
+++ graphreport/runner.py
@@ -0,0 +1,87 @@
+import os
+import argparse
+from .graphreport import generate_report
+from .graphreport import IGNORE_TYPES
+from .graphreport import IGNORE_LIBRARIES
+from .version import __version__
+
+
+def parse_options():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    general = parser.add_argument_group("General")
+    parser.add_argument(
+        '-v', '--version',
+        action='store_true',
+        dest='version',
+        help='Display application version information'
+    )
+    general.add_argument(
+        '--logo',
+        dest='logo',
+        default='https://www.freepnglogos.com/uploads/cisco-png-logo/new-cisco-logo-png-1.png',
+        help="User logo (default: dummy image )"
+    )
+
+    general.add_argument(
+        '--ignorelib',
+        dest='ignore',
+        default=IGNORE_LIBRARIES,
+        nargs="+",
+        help="Ignore keywords of specified library in report"
+    )
+
+    general.add_argument(
+        '--ignoretype',
+        dest='ignoretype',
+        default=IGNORE_TYPES,
+        nargs="+",
+        help="Ignore keywords with specified type in report"
+    )
+
+    general.add_argument(
+        '-I', '--inputpath',
+        dest='path',
+        default=os.path.curdir,
+        help="Path of result files"
+    )
+
+    general.add_argument(
+        '-M', '--metrics-report-name',
+        dest='metrics_report_name',
+        help="Output name of the generate metrics report"
+    )
+
+    general.add_argument(
+        '-R', '--report',
+        dest='report_name',
+        default='report.html',
+        help="Name of report.html"
+    )
+
+    general.add_argument(
+        '-L', '--log',
+        dest='log_name',
+        default='log.html',
+        help="Name of log.html"
+    )
+
+    general.add_argument(
+        '-O', '--output',
+        dest='output',
+        default="output.xml",
+        help="Name of output.xml"
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_options()
+
+    if args.version:
+        print(__version__)
+        exit(0)
+
+    generate_report(args)
